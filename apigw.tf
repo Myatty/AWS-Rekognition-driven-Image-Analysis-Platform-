8b9dda4804ec5380bc90b8c9a33d1a6f592af6b5
Myatty <myintmyatps664@gmail.com>
2025-10-24 12:55:34 +0630

    update apigw.tf to add new resource path

--- apigw.tf
+++ apigw.tf
@@ -6,3 +6,9 @@ resource "aws_api_gateway_rest_api" "entry_rest_api_gw" {
       types = ["REGIONAL"]
     }
 }
+
+resource "aws_api_gateway_resource" "root_resource_path" {
+    rest_api_id = aws_api_gateway_rest_api.entry_rest_api_gw.id
+    parent_id = aws_api_gateway_rest_api.entry_rest_api_gw.root_resource_id
+    path_part = "analyzePhoto"
+}
